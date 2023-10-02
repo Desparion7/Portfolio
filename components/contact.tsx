a842ed7dcbcda6b1cdb76cbfcf8f99fce0fe1570
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-10-02 14:04:18 +0200

    add footer

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react';
 import SectionHeading from './section-heading';
-import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
 import { sendEmail } from '@/actions/sendEmail';
+import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
 
 const Contact = () => {
 	const { ref } = useSectionInView('Contact');
@@ -36,7 +37,12 @@ const Contact = () => {
 			<form
 				className='mt-10 flex flex-col'
 				action={async (formData) => {
-					await sendEmail(formData);
+					const { data, error } = await sendEmail(formData);
+					if (error) {
+						toast.error(error);
+						return;
+					}
+					toast.success('Email sent successfully');
 				}}
 			>
 				<input
@@ -54,13 +60,7 @@ const Contact = () => {
 					required
 					maxLength={5000}
 				/>
-				<button
-					type='submit'
-					className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all  focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105'
-				>
-					Submit
-					<FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1' />
-				</button>
+				<SubmitBtn />
 			</form>
 		</motion.section>
 	);
