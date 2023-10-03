e02e7341502f3198d503be1cbec58d1acc6d6dfd
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-10-03 09:44:59 +0200

    add theme switch

--- components/experience.tsx
+++ components/experience.tsx
@@ -8,9 +8,11 @@ import {
 import 'react-vertical-timeline-component/style.min.css';
 import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
+import { useThemeContext } from '@/context/theme-context';
 
 const Experience = () => {
 	const { ref } = useSectionInView('Experience');
+	const { theme } = useThemeContext();
 	return (
 		<section
 			ref={ref}
@@ -24,19 +26,28 @@ const Experience = () => {
 						<React.Fragment key={index}>
 							<VerticalTimelineElement
 								contentStyle={{
-									background: '#f3f4f6',
+									background:
+										theme === 'light'
+											? '#f3f4f6'
+											: 'rgba(255,255,255, 0.05)',
 									boxShadow: 'none',
 									border: '1px solid rgba(0,0,0,0.05)',
 									textAlign: 'left',
 									padding: '1.3rem 2rem',
 								}}
 								contentArrowStyle={{
-									borderRight: '0.4rem solid #9ca3af',
+									borderRight:
+										theme === 'light'
+											? '0.4rem solid #9ca3af'
+											: '0.4rem solid rgba(255,255,255,0.5)',
 								}}
 								date={item.date}
 								icon={item.icon}
 								iconStyle={{
-									background: 'white',
+									background:
+										theme === 'light'
+											? 'white'
+											: 'rgba(255,255,255,0.15)',
 									fontSize: '1.5rem',
 								}}
 							>
@@ -46,7 +57,7 @@ const Experience = () => {
 								<p className='font-normal !mt-0'>
 									{item.location}
 								</p>
-								<p className='!mt-1 !font-normal text-gray-700'>
+								<p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>
 									{item.description}
 								</p>
 							</VerticalTimelineElement>
---
+++ context/theme-context.tsx
@@ -0,0 +1,64 @@
+'use client';
+import React, {
+	useEffect,
+	useState,
+	ReactNode,
+	createContext,
+	useContext,
+} from 'react';
+
+type Theme = 'light' | 'dark';
+
+type ThemeContextProviderProps = {
+	children: ReactNode;
+};
+
+type ThemeContextType = {
+	theme: Theme;
+	toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+const ThemeContextProvider = ({ children }: ThemeContextProviderProps) => {
+	const [theme, setTheme] = useState<Theme>('light');
+
+	const toggleTheme = () => {
+		if (theme === 'light') {
+			setTheme('dark');
+			window.localStorage.setItem('theme', 'dark');
+			document.documentElement.classList.add('dark');
+		} else {
+			setTheme('light');
+			window.localStorage.setItem('theme', 'light');
+			document.documentElement.classList.remove('dark');
+		}
+	};
+
+	useEffect(() => {
+		const localTheme = window.localStorage.getItem('theme') as Theme | null;
+		if (localTheme) {
+			setTheme(localTheme);
+			if (localTheme === 'dark')
+				document.documentElement.classList.add('dark');
+		} else if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
+			setTheme('dark');
+			document.documentElement.classList.add('dark');
+		}
+	}, []);
+	return (
+		<ThemeContext.Provider value={{ theme, toggleTheme }}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
+
+export const useThemeContext = () => {
+	const context = useContext(ThemeContext);
+	if (!context) {
+		throw new Error('useThemeContext must be use in ThemeContextProvider');
+	}
+	return context;
+};
+
+export default ThemeContextProvider;
