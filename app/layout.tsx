12fa1a0ac10312e2291af3bd9b30e6ac49e3a976
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-12-08 20:06:02 +0100

    move first check for mode from layout to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,20 @@
 'use client';
 import Header from '@/components/header';
 import './globals.css';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
 import Footer from '@/components/footer';
 import ThemeSwitch from '@/components/theme-switch';
-import { Theme, useThemeStore } from '@/context/theme-context';
-import { useEffect } from 'react';
+
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-	title: 'Mateusz Woś | Portfolio',
-	description: 'Mateusz Woś portfolio page',
-};
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const setThemeSection = useThemeStore((state) => state.setThemeSection);
 
-	const localTheme = window.localStorage.getItem('theme') as Theme;
-	useEffect(() => {
-		if (localTheme) {
-			setThemeSection(localTheme);
-			if (localTheme === 'dark')
-				document.documentElement.classList.add('dark');
-		} else if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
-			setThemeSection('dark');
-			document.documentElement.classList.add('dark');
-		}
-	}, [setThemeSection, localTheme]);
 
 	return (
 		<html lang='en' className='!scroll-smooth'>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import About from '@/components/about';
 import Contact from '@/components/contact';
 import Experience from '@/components/experience';
@@ -5,8 +6,22 @@ import Intro from '@/components/intro';
 import Projects from '@/components/projects';
 import SectionDivider from '@/components/section-divider';
 import Skills from '@/components/skills';
+import { useEffect } from 'react';
+import { Theme, useThemeStore } from '@/context/theme-context';
 
 export default function Home() {
+	const setThemeSection = useThemeStore((state) => state.setThemeSection);
+	const localTheme = window.localStorage.getItem('theme') as Theme;
+	useEffect(() => {
+		if (localTheme) {
+			setThemeSection(localTheme);
+			if (localTheme === 'dark')
+				document.documentElement.classList.add('dark');
+		} else if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
+			setThemeSection('dark');
+			document.documentElement.classList.add('dark');
+		}
+	}, [setThemeSection, localTheme]);
 	return (
 		<main className='flex flex-col items-center px-4'>
 			<Intro />
